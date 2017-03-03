df305f2bb42929c5782b3d269882a2164af9994e
Derek-Ashmore <dashmore@force66.com>
2017-03-03 05:43:14 -0600

    refactored Network ACL code for DMZ and private subnets

--- terraform-modules/aws-vpc/variables.tf
+++ terraform-modules/aws-vpc/variables.tf
@@ -19,10 +19,11 @@ variable "cidr_block_vpc_suffix" {
 	default = "/16"
 }
 
-variable "cidr_block_public_subnet_suffix" {
+/*variable "cidr_block_public_subnet_suffix" {
 	description = "CIDR block suffix (two nodes plus slash) for public subnets"
 	default = ".240.0/20"
-}
+}*/
+
 variable "cidr_block_public_subnet_segment_suffix_list" {
   type    = "list"
 	description = "CIDR block suffix (two nodes plus slash) for individual public subnets"
@@ -35,8 +36,20 @@ variable "cidr_block_dmz_subnet_segment_suffix_list" {
   default = [".96.0/19", ".128.0/19", ".160.0/19"]
 }
 
+variable "cidr_block_dmz_subnet_allowed_ingress_cidr_list" {
+  type    = "list"
+	description = "CIDR blocks allowed into DMZ subnets"
+  default = ["10.0.96.0/19", "10.0.128.0/18", "10.0.240.0/21", "10.0.248.0/22"]
+}
+
 variable "cidr_block_private_subnet_segment_suffix_list" {
   type    = "list"
 	description = "CIDR block suffix (two nodes plus slash) for individual private subnets"
   default = [".0.0/19", ".32.0/19", ".64.0/19"]
+}
+
+variable "cidr_block_private_subnet_allowed_ingress_cidr_list" {
+  type    = "list"
+	description = "CIDR blocks allowed into Private subnets"
+  default = ["10.0.0.0/17", "10.0.128.0/18"]
 }
--- terraform-modules/aws-vpc/vpc.tf
+++ terraform-modules/aws-vpc/vpc.tf
@@ -126,6 +126,7 @@ resource "aws_network_acl" "privateSubnetsNetworkACL" {
     to_port = 65535
   }
 
+/*
   # Part 1: Allow all inbound traffic from other private/DMZ subnets (10.0.0.0 - 10.0.191.255)
   ingress {
     protocol = "all"
@@ -145,16 +146,28 @@ resource "aws_network_acl" "privateSubnetsNetworkACL" {
     from_port = 0
     to_port = 65535
   }
-
+*/
   # Deny all inbound traffic **NOT** from other private/DMZ subnets (10.0.0.0 - 10.0.191.255)
   ingress {
     protocol = "all"
-    rule_no = 300
+    rule_no = 900
     action = "deny"
     cidr_block =  "0.0.0.0/16"
     from_port = 0
     to_port = 65535
   }
+}
+
+resource "aws_network_acl_rule" "privateSubnetsNetworkACLRules" {
+  network_acl_id = "${aws_network_acl.privateSubnetsNetworkACL.id}"
+  count = "${length(var.cidr_block_private_subnet_allowed_ingress_cidr_list)}"
+  rule_number = "2${count.index}0"
+  egress = false
+  protocol = "-1"
+  rule_action = "allow"
+  cidr_block = "${var.cidr_block_private_subnet_allowed_ingress_cidr_list[count.index]}"
+  from_port = 0
+  to_port = 65535
 }
 
 # Find all DMZ subnets
@@ -194,7 +207,7 @@ resource "aws_network_acl" "dmzSubnetsNetworkACL" {
     from_port = 0
     to_port = 65535
   }
-
+/*
   # Part 2: Allow all inbound traffic from DMZ subnets (10.0.96.0 - 10.0.191.255)
   ingress {
     protocol = "all"
@@ -223,17 +236,29 @@ resource "aws_network_acl" "dmzSubnetsNetworkACL" {
     cidr_block =  "10.0.248.0/22"
     from_port = 0
     to_port = 65535
-  }
+  }*/
 
   # Deny all inbound traffic **NOT** from other DMZ/Public subnets (10.0.96.0 - 10.0.191.255, 10.0.240.0 - 10.0.251.255)
   ingress {
     protocol = "all"
-    rule_no = 500
+    rule_no = 900
     action = "deny"
     cidr_block =  "0.0.0.0/16"
     from_port = 0
     to_port = 65535
   }
+}
+
+resource "aws_network_acl_rule" "dmzSubnetsNetworkACLRules" {
+  network_acl_id = "${aws_network_acl.dmzSubnetsNetworkACL.id}"
+  count = "${length(var.cidr_block_dmz_subnet_allowed_ingress_cidr_list)}"
+  rule_number = "2${count.index}0"
+  egress = false
+  protocol = "-1"
+  rule_action = "allow"
+  cidr_block = "${var.cidr_block_dmz_subnet_allowed_ingress_cidr_list[count.index]}"
+  from_port = 0
+  to_port = 65535
 }
 
 # Find all DMZ subnets
