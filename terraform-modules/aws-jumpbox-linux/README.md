e33a5ed80e35105b6e91098697578f49bcd018c8
Derek-Ashmore <dashmore@force66.com>
2017-03-13 05:52:40 -0500

    Fixed security group issue

--- terraform-modules/aws-jumpbox-linux/README.md
+++ terraform-modules/aws-jumpbox-linux/README.md
@@ -5,3 +5,6 @@ in your region with a name starting with "amzn-ami-hvm".
 
 This module illustrates searching listed AMIs for the most recent release of a specific type
 of image.
+
+Typically, jump box VMs are placed in a DMZ subnet and not directly accessible via the
+internet. Usually, you should utilize some type of VPN solution for access to it.
--- terraform-modules/aws-jumpbox-linux/jumpbox.tf
+++ terraform-modules/aws-jumpbox-linux/jumpbox.tf
@@ -8,12 +8,39 @@
    region     = "${var.aws_region}"
  }
 
+ resource "aws_security_group" "jumpBoxSecurityGroup" {
+  name = "${var.instance_name} - Linux Jump Box Standard"
+  description = "Allow ssh and sftp in - anything out"
+  vpc_id = "${data.aws_subnet.targetSubnet.vpc_id}"
+}
+
+resource "aws_security_group_rule" "allowSsh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = "${aws_security_group.jumpBoxSecurityGroup.id}"
+}
+
+resource "aws_security_group_rule" "allowAllEgress" {
+    type = "egress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = "${aws_security_group.jumpBoxSecurityGroup.id}"
+}
+
  resource "aws_instance" "jumpBox" {
    ami           = "${data.aws_ami.linux_ami.id}"
    instance_type = "t2.micro"
    subnet_id  = "${var.subnet_id}"
    key_name = "${var.key_pair}"
    associate_public_ip_address = "${var.assignPublicIp}"
+   vpc_security_group_ids = ["${aws_security_group.jumpBoxSecurityGroup.id}"]
 
    tags {
      "Name" = "${var.instance_name} - ${data.aws_subnet.targetSubnet.tags.Name}"
